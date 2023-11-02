eecf5af828dee94b2add88de38aee91f2ac99e02
Acer <Acer@DESKTOP-JS0O9TD>
2023-11-02 23:34:13 +0330

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,95 @@
+﻿//نیما فدوی
+//میانگین 10 دانشجو
+
+Console.WriteLine("nomre 1 ra vared konid");
+string input1 = Console.ReadLine();
+int x = Convert.ToInt32(input1);
+Console.WriteLine("zarib nomre aval ra vared konid");
+string n1 = Console.ReadLine();
+int zarib1 = Convert.ToInt32(n1);
+int nomre1 = zarib1 * x;
+Console.WriteLine(nomre1);
+
+Console.WriteLine("nomre 2 ra vared konid");
+string input2 = Console.ReadLine();
+int U = Convert.ToInt32(input2);
+Console.WriteLine("ZARIB NOMRE 2 RA VARED KONID");
+String n2 = Console.ReadLine();
+int zarib2 = Convert.ToInt32(n2);
+int nomre2 = zarib2 * U;
+Console.WriteLine(nomre2);
+
+Console.WriteLine("nomre 3 ra vared konid ");
+string input3 = Console.ReadLine();
+int y = Convert.ToInt32(input3);
+Console.WriteLine("zarib nomre 3 ra vared konid");
+string n3 = Console.ReadLine();
+int zarib3 = Convert.ToInt32(n3);
+int nomre3 = zarib3 * y;
+Console.WriteLine(nomre3);
+
+Console.WriteLine("nomre4 ra vared konid");
+string input4 = Console.ReadLine();
+int s = Convert.ToInt32(input4);
+Console.WriteLine("Zarib nomre4 ra vared konid ");
+string n4 = Console.ReadLine();
+int zarib4 = Convert.ToInt32(n4);
+int nomre4 = zarib4 * s;
+Console.WriteLine(nomre4);
+
+Console.WriteLine("nomre5 ra vared konid");
+string input5 = Console.ReadLine();
+int f = Convert.ToInt32(input5);
+Console.WriteLine("zarib nomre 5 ra vared konid");
+string n5 = Console.ReadLine();
+int zarib5 = Convert.ToInt32(n5);
+int nomre5 = zarib5 * f;
+Console.WriteLine(nomre5);
+
+Console.WriteLine("nomre6 ra vared konid");
+string input6 = Console.ReadLine();
+int z = Convert.ToInt32(input6);
+Console.WriteLine(" zarib nomre6 ra vared konid");
+string n6 = Console.ReadLine();
+int zarib6 = Convert.ToInt32(n6);
+int nomre6 = zarib6 * z;
+Console.WriteLine(nomre6);
+
+Console.WriteLine("nomre7 ra vared konud");
+string input7 = Console.ReadLine();
+int r = Convert.ToInt32(input7);
+Console.WriteLine("zarib nomre7 ra vared konid");
+string n7 = Console.ReadLine();
+int zarib7 = Convert.ToInt32(n7);
+int nomre7 = zarib7 * r;
+Console.WriteLine(nomre7);
+
+Console.WriteLine("nomre8 ra vared konid");
+string input8 = Console.ReadLine();
+int d = Convert.ToInt32(input8);
+Console.WriteLine("zarib nomre8 ra vared konid");
+string n8 = Console.ReadLine();
+int zarib8 = Convert.ToInt32(n8);
+int nomre8 = zarib8 * d;
+Console.WriteLine(nomre8);
+
+Console.WriteLine("nomre 9 ra vared konid");
+string input9 = Console.ReadLine();
+int b = Convert.ToInt32(input9);
+Console.WriteLine("zarib nomre9 ra vared konid");
+string n9 = Console.ReadLine();
+int zarib9 = Convert.ToInt32(n9);
+int nomre9 = zarib9 * b;
+Console.WriteLine(nomre9);
+
+Console.WriteLine("nomre10 ra vared konid");
+string input10 = Console.ReadLine();
+int h = Convert.ToInt32(input10);
+Console.WriteLine("zarib nomre10 ra vared konid");
+string n10 = Console.ReadLine();
+int zarib10 = Convert.ToInt32(n10);
+int nomre10 = zarib10 * h;
+Console.WriteLine(nomre10);
+
+int miangin = (nomre1 + nomre2 + nomre3 + nomre4 + nomre5 + nomre6 + nomre7 + nomre8 + nomre9 + nomre10) / (zarib1 + zarib2 + zarib3 + zarib4 + zarib5 + zarib6 + zarib7 + zarib8 + zarib9 + zarib10);
+Console.ReadKey();
